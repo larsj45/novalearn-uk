81e57591425bf286c1fd711b925b8b9f28276196
Lars Janér <larsjaner@Larss-MacBook-Air-2.local>
2026-02-09 12:57:25 -0300

    fix: remove hardcoded Stripe API version that may be invalid

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,5 +3,5 @@ import Stripe from 'stripe'
 const stripeKey = process.env.STRIPE_SECRET_KEY
 
 export const stripe = stripeKey
-  ? new Stripe(stripeKey, { apiVersion: '2025-12-18.acacia' as Stripe.LatestApiVersion })
+  ? new Stripe(stripeKey)
   : null as unknown as Stripe
